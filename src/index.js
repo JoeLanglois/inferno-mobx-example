c6db3a65cc447a1fbdbcf6763bd22cfb7740049c
Joe Des Ruisseaux-Langlo <Joe@MacBook-Pro-de-Joe.local>
2016-03-10 09:04:47 -0500

    Added mobx

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import Inferno from 'inferno';
 import InfernoDOM from 'inferno-dom';
 
+import {autorun, observable} from 'mobx'
+
+let NameStore = observable({
+    name: 'Joe'
+})
+
+
+const HelloWorld  = () => {
+    return <h1>Hello {NameStore.name}</h1>
+}   
+
+autorun(() => {
+    InfernoDOM.render(
+        <HelloWorld name={NameStore.name} />,
+        document.body
+    )
+})
+
+setTimeout(() => {
+    NameStore.name = 'Adèle'
+}, 1000)
+
+autorun(() => {
+    console.log("Name changed: " + NameStore.name)
+})
